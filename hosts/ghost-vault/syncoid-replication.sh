4df639ffb07308ed8f9206f1b7b856aa5554f07b
pypeaday <pypeaday@pype.dev>
2024-12-13 20:33:59 -0600

    add options to syncoid replication back to ghost-vault

--- hosts/ghost-vault/syncoid-replication.sh
+++ hosts/ghost-vault/syncoid-replication.sh
@@ -15,7 +15,7 @@ curl https://hc-ping.com/uWDfVXr2W4O9rF7deuOEog/syncoid/start
 # Really only need this because I have the "encrypted" parent dataset from not
 # using encryption in the beginning. This makes sure though that tank/encrytped
 # will exist on the backup
-syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation nic@ghost:tank/encrypted tank/encrypted
+syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation --create-bookmark --no-resume nic@ghost:tank/encrypted tank/encrypted
 
 # echo "Syncing nic@ghost:tank/encrypted/10Fold!"
 # this dataset sometimes causes issues but I don't need it anyways
@@ -27,24 +27,24 @@ syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation nic@ghost:tank/e
 
 echo "Syncing nic@ghost:tank/encrypted/vms!"
 # syncoid --skip-parent --no-sync-snap --sendoptions=w -r --no-privilege-elevation nic@ghost:tank/encrypted/vms tank/encrypted/vms
-syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation nic@ghost:tank/encrypted/vms tank/encrypted/vms
-syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation nic@ghost:tank/encrypted/vms/pihole tank/encrypted/vms/pihole
-syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation nic@ghost:tank/encrypted/vms/homeassistant tank/encrypted/vms/homeassistant
+syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation --create-bookmark --no-resume nic@ghost:tank/encrypted/vms tank/encrypted/vms
+syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation --create-bookmark --no-resume nic@ghost:tank/encrypted/vms/pihole tank/encrypted/vms/pihole
+syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation --create-bookmark --no-resume nic@ghost:tank/encrypted/vms/homeassistant tank/encrypted/vms/homeassistant
 # syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation nic@ghost:tank/encrypted/vms/win10 tank/encrypted/vms/win10
 # syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation nic@ghost:tank/encrypted/vms/block-subnet-router tank/encrypted/vms/block-subnet-router
 
 echo "Syncing nic@ghost:tank/encrypted/docker!"
-syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation nic@ghost:tank/encrypted/docker tank/encrypted/docker
+syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation --create-bookmark --no-resume nic@ghost:tank/encrypted/docker tank/encrypted/docker
 
 echo "Syncing nic@ghost:tank/encrypted/docker/nextcloud-zfs!"
 syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation nic@ghost:tank/encrypted/docker/nextcloud-zfs tank/encrypted/docker/nextcloud-zfs
 
 echo "Syncing nic@ghost:tank/encrypted/nas! excluding tank/encrypted/nas/media"
-syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation nic@ghost:tank/encrypted/nas tank/encrypted/nas
-syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation nic@ghost:tank/encrypted/nas/documents tank/encrypted/nas/documents
-syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation nic@ghost:tank/encrypted/nas/documents/paperless tank/encrypted/nas/documents/paperless
-syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation nic@ghost:tank/encrypted/nas/dump tank/encrypted/nas/dump
-syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation nic@ghost:tank/encrypted/nas/torrents tank/encrypted/nas/torrents
+syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation --create-bookmark --no-resume nic@ghost:tank/encrypted/nas tank/encrypted/nas
+syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation --create-bookmark --no-resume nic@ghost:tank/encrypted/nas/documents tank/encrypted/nas/documents
+syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation --create-bookmark --no-resume nic@ghost:tank/encrypted/nas/documents/paperless tank/encrypted/nas/documents/paperless
+syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation --create-bookmark --no-resume nic@ghost:tank/encrypted/nas/dump tank/encrypted/nas/dump
+syncoid --no-sync-snap --sendoptions=w --no-privilege-elevation --create-bookmark --no-resume nic@ghost:tank/encrypted/nas/torrents tank/encrypted/nas/torrents
 
 # hcio
 curl -m 10 --retry 5 https://hc-ping.com/uWDfVXr2W4O9rF7deuOEog/syncoid
